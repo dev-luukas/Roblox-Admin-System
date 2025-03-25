da04a6ed83d0f1afc5e1eed3502324d66e0090fd
Lukas <160350072+dev-luukas@users.noreply.github.com>
2025-03-25 20:15:48 +0100

    Update IsAdmin.ts

--- src/shared/IsAdmin.ts
+++ src/shared/IsAdmin.ts
@@ -2,6 +2,6 @@ import { Players } from "@rbxts/services";
 
 const Moderators = new Set<number>([]);
 const Developers = new Set<number>([]);
-const Owners = new Set<number>([5012441109]);
+const Owners = new Set<number>([]);
 
 export default { Moderators, Owners, Developers };
